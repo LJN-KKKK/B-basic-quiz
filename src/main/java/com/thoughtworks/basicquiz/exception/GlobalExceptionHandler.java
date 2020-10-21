fdad5e00aa7ca77e43d45d150873bf8103fadc54
Jianan.Li <jianan.li@thoughtworks.com>
2020-10-21 19:32:00 +0800

    modify timestamp format

--- src/main/java/com/thoughtworks/basicquiz/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/basicquiz/exception/GlobalExceptionHandler.java
@@ -5,15 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.TimeZone;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler({UserHasNoEducationException.class, UserNotExistException.class})
     public ResponseEntity<ErrorResult> handle(RuntimeException ex) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sssZ", Locale.CHINA);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         ErrorResult errorResult = new ErrorResult(
-                new Date().toString(),
+                sdf.format(new Date()),
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND.name(),
                 ex.getMessage());
@@ -22,9 +28,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResult> handle(MethodArgumentNotValidException ex) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sssZ", Locale.CHINA);
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
         String message = Objects.requireNonNull(ex.getBindingResult().getFieldError()).getDefaultMessage();
         ErrorResult errorResult = new ErrorResult(
-                new Date().toString(),
+                sdf.format(new Date()),
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.name(),
                 message);
